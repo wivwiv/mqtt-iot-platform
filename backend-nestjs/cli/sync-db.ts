68e57a98862de748f4577f40c1687fea208934ed
wivwiv <bigpar@qq.com>
2020-02-11 22:19:33 +0800

    rename project name

--- backend-nestjs/cli/sync-db.ts
+++
@@ -1,7 +0,0 @@
-import { DatabaseService } from "../src/database/database.service";
-import { ConfigService } from "../src/config/config.service";
-const configService = new ConfigService('.env');
-const databaseService = new DatabaseService();
-databaseService.initDB().then(() => {
-    databaseService.sync(false);
-});
